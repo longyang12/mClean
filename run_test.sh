769e05e7c8a1e826feaf1cfe981e92be42defb77
long <long@longdeMacBook-Pro.local>
2026-03-08 16:24:59 +0800

    add run at mac

---
+++ run_test.sh
@@ -0,0 +1,5 @@
+
+python tools/process_data.py --config /Users/long/Desktop/projects/mClean/data-recipes/redpajama-arxiv-refine-cpu-np1-nocache.yaml
+
+# python tools/process_data.py --config /Users/long/Desktop/projects/mClean/data-recipes/redpajama-arxiv-refine-cpu-np1-cache.yaml
+
